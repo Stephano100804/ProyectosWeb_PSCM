9b69988f616e65e55025d4c0da54629e9199d92f
Paul Stephano Cueva <cuevastephano1008@gmail.com>
2025-07-09 17:13:14 -0500

    2do Avance - Configuración para los Procedimientos Almacenados y Consultas SQL

---
+++ ProyectosWeb/Datos/DNG_ad_tmusua/DNG_ad_tmusua.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.ApplicationBlocks.Data;
+using ProyectosWeb.Clases;
+using ProyectosWeb.Entidades.ENT_MCAdmin;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace ProyectosWeb.Datos.DNG_ad_tmusua
+{
+    public class DNG_ad_tmusua
+    {
+        public static DataTable Login(E_ad_tmusua usua)
+        {
+            return SqlHelper.ExecuteDataTable(Cls_conexion.cadena, "sp_ad_tmusua_Q01", usua.co_usua, usua.no_clav);
+        }
+    }
+}
---
+++ ProyectosWeb/Entidades/ENT_MCAdmin/E_ad_tmusua.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProyectosWeb.Entidades.ENT_MCAdmin
+{
+    public class E_ad_tmusua
+    {
+        public int condicion { get; set; }
+        public string co_usua { get; set; }
+        public string co_grup { get; set; }
+        public string co_perf { get; set; }
+        public string no_usua { get; set; }
+        public string no_clav { get; set; }
+        public string nu_telef1 { get; set; }
+        public string nu_telef2 { get; set; }
+        public string de_correo1 { get; set; }
+        public string de_correo2 { get; set; }
+        public string co_sucu_usuario { get; set; }
+        public string no_iniciales_usu { get; set; }
+        public string st_usua { get; set; }
+        public Int16 st_registro { get; set; }
+        public string co_usua_crea { get; set; }
+        public DateTime fe_usua_crea { get; set; }
+        public string co_usua_modi { get; set; }
+        public DateTime fe_usua_modi { get; set; }
+        public string StrVariable { get; set; }
+    }
+}
